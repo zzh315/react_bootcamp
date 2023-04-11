59ec8e997aaa5511c26f03ef74644d4a947e7acf
zzh315 <zhengzh315@gmail.com>
2023-04-11 17:06:50 +1000

    react section3 finished Day2

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,35 +1,34 @@
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
+import Card from "../UI/Card";
 
 function Expenses(props) {
   return (
-    <div className="expenses">
-      [
+    <Card className="expenses">
       <ExpenseItem
         title={props.expense[0].title}
         amount={props.expense[0].amount}
         date={props.expense[0].date}
       />
-      ,
+
       <ExpenseItem
         title={props.expense[1].title}
         amount={props.expense[1].amount}
         date={props.expense[1].date}
       />
-      ,
+
       <ExpenseItem
         title={props.expense[2].title}
         amount={props.expense[2].amount}
         date={props.expense[2].date}
       />
-      ,
+
       <ExpenseItem
         title={props.expense[3].title}
         amount={props.expense[3].amount}
         date={props.expense[3].date}
       />
-      ,]
-    </div>
+    </Card>
   );
 }
 
